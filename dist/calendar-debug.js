719bdb4bf295b460f16125b8d043c94455c89d3d
lepture <lepture@me.com>
2012-10-30 15:23:40 +0800

    update dependencies

--- dist/calendar-debug.js
+++ dist/calendar-debug.js
@@ -1,7 +1,7 @@
-define("#calendar/0.8.0/model-debug", ["$-debug", "#base/1.0.0/base-debug", "#class/1.0.0/class-debug", "#events/1.0.0/events-debug", "#moment/1.6.2/moment-debug"], function(require, exports, module) {
+define("#calendar/0.8.0/model-debug", ["$-debug", "#base/1.0.1/base-debug", "#class/1.0.0/class-debug", "#events/1.0.0/events-debug", "#moment/1.6.2/moment-debug"], function(require, exports, module) {
 
     var $ = require('$-debug');
-    var Base = require('#base/1.0.0/base-debug');
+    var Base = require('#base/1.0.1/base-debug');
     var moment = require('#moment/1.6.2/moment-debug');
 
     // Create a model data on calendar. For example, now is May, 2012.
@@ -360,7 +360,7 @@ define("#calendar/0.8.0/model-debug", ["$-debug", "#base/1.0.0/base-debug", "#cl
 //
 // ## Syntax Overview:
 //
-//     var Calendar = require('undefined-debug');
+//     var Calendar = require('calendar-debug');
 //     var cal = new Calendar({
 //         trigger: 'input.date-picker',
 //         format: "YYYY-MM-DD"
@@ -369,13 +369,13 @@ define("#calendar/0.8.0/model-debug", ["$-debug", "#base/1.0.0/base-debug", "#cl
 // Need more complex task? Head over to Options section.
 //
 
-define("#calendar/0.8.0/calendar-debug", ["./model-debug", "$-debug", "#moment/1.6.2/moment-debug", "#overlay/0.9.9/overlay-debug", "#position/0.9.2/position-debug", "#iframe-shim/0.9.3/iframe-shim-debug", "#widget/0.9.16/widget-debug", "#base/0.9.16/base-debug", "#events/0.9.1/events-debug", "#class/0.9.2/class-debug", "#widget/1.0.0/templatable-debug", "#handlebars/1.0.0/handlebars-debug", "i18n!lang-debug", "#base/1.0.0/base-debug", "#class/1.0.0/class-debug", "#events/1.0.0/events-debug"], function(require, exports, module) {
+define("#calendar/0.8.0/calendar-debug", ["./model-debug", "$-debug", "#moment/1.6.2/moment-debug", "#overlay/0.9.11/overlay-debug", "#position/1.0.0/position-debug", "#iframe-shim/1.0.0/iframe-shim-debug", "#widget/1.0.2/widget-debug", "#base/1.0.1/base-debug", "#class/1.0.0/class-debug", "#events/1.0.0/events-debug", "#widget/1.0.2/templatable-debug", "#handlebars/1.0.0/handlebars-debug", "i18n!lang-debug"], function(require, exports, module) {
 
     // Calendar is designed for desktop, we don't need to consider ``zepto``.
     var $ = require('$-debug');
     var moment = require('#moment/1.6.2/moment-debug');
-    var Overlay = require('#overlay/0.9.9/overlay-debug');
-    var Templatable = require('#widget/1.0.0/templatable-debug');
+    var Overlay = require('#overlay/0.9.11/overlay-debug');
+    var Templatable = require('#widget/1.0.2/templatable-debug');
     var lang = require('i18n!lang-debug') || {};
 
     var template = '<div class="ui-calendar"><ul class="ui-calendar-navigation" data-role="navigation-container"><li class="ui-calendar-previous-year" data-role="prev-year">&lt;&lt;</li><li class="ui-calendar-previous-month" data-role="prev-month">&lt;</li><li class="ui-calendar-month-year" data-role="month-year-container"><span class="month" data-role="mode-month" data-value="{{month.current.value}}">{{_ month.current.label}}</span><span class="year" data-role="mode-year">{{year.current.label}}</span></li><li class="ui-calendar-next-month" data-role="next-month">&gt;</li><li class="ui-calendar-next-year" data-role="next-year">&gt;&gt;</li></ul><ul class="ui-calendar-control" data-role="pannel-container">{{#if mode.date}}{{#each day.items}}<li class="ui-calendar-day ui-calendar-day-{{value}}" data-role="day" data-value="{{value}}">{{_ label}}</li>{{/each}}{{/if}}</ul><div class="ui-calendar-data-container" data-role="data-container">{{#if mode.date}}{{#each date.items}}<ul class="ui-calendar-date-column">{{#each this}}<li class="ui-calendar-day-{{day}} {{className}}{{#unless available}}disabled-date{{/unless}}"data-role="date" data-value="{{value}}">{{label}}</li>{{/each}}</ul>{{/each}}{{/if}}{{#if mode.month}}{{#each month.items}}<ul class="ui-calendar-month-column">{{#each this}}<li data-role="month" data-value="{{value}}">{{_ label}}</li>{{/each}}</ul>{{/each}}{{/if}}{{#if mode.year}}{{#each year.items}}<ul class="ui-calendar-year-column">{{#each this}}<li data-role="{{role}}" data-value="{{value}}">{{_ label}}</li>{{/each}}</ul>{{/each}}{{/if}}</div><ul class="ui-calendar-footer" data-role="time-container"><li class="ui-calendar-today" data-role="today">{{_ message.today}}</li>{{#if mode.time}}<li class="ui-calendar-time" colspan="2" data-role="time"><span class="ui-calendar-hour">{{time.hour}}</span> : {{time.minute}}</li>{{/if}}</ul></div>';
