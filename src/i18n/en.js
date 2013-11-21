b5936ce56984d64cf7114fb65f382ce9b77d9ba1
afc163 <afc163@gmail.com>
2013-11-21 11:04:41 +0800

    fix #55

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,1 +1,22 @@
-define({});
+define({
+  'Su': 'S',
+  'Mo': 'M',
+  'Tu': 'T',
+  'We': 'W',
+  'Th': 'T',
+  'Fr': 'F',
+  'Sa': 'S',
+  'Jan': 'Jan',
+  'Feb': 'Feb',
+  'Mar': 'Mar',
+  'Apr': 'Apr',
+  'May': 'May',
+  'Jun': 'Jun',
+  'Jul': 'Jul',
+  'Aug': 'Aug',
+  'Sep': 'Sep',
+  'Oct': 'Oct',
+  'Nov': 'Nov',
+  'Dec': 'Dec'
+});
+
